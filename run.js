c2b6fefad417ad06b0df5914eccf78d523c65bf3
Trevor <trevor@wallsmetalroofing.com>
2020-11-25 09:25:30 -0400

    Added a update command

--- run.js
+++ run.js
@@ -4,52 +4,77 @@ const ping = require('ping');
 const chalk = require("chalk");
 
 const myArgs = process.argv.slice(2);
-const hosts = myArgs.length ? myArgs : ['1.1.1.1', '8.8.8.8'];
-/**
- * @type {{[key: string]: Date}}
- */
-let outages = {};
+if (myArgs[0] === "update") {
 
-console.log(chalk.green("Starting"), chalk.blue((new Date()).toLocaleTimeString()));
-console.log(...hosts.map(host => chalk.blue(host)));
-console.log("");
+    const command = "npm i -g git+https://github.com/wallsmetalroofing/wmr-internet-ping";
+    console.log(chalk.grey("> " + command));
 
-setInterval(async () => {
+    const {
+        exec
+    } = require("child_process");
 
-    const results = await Promise.all(hosts.map(async host => {
-        try {
-
-            const result = await ping.promise.probe(host, {
-                timeout: 0.8
-            });
-
-            if (!result.alive) {
-                throw new Error("Down");
-            }
-
-
-            if (outages[host]) {
-                // calculate the outage time
-                let elapsed = (new Date()).getTime() - outages[host].getTime();
-
-                console.log("");
-                console.log(chalk.blue(host), chalk.grey("out for"), chalk.red(Math.round(elapsed / 1000)), chalk.grey("seconds"));
-                outages[host] = null;
-            }
-
-            return chalk.blue.bold(host) + chalk.grey(" - ") + chalk.green(result.time + "ms");
-
-        } catch (err) {
-            // ping failed
-            if (!outages[host]) {
-                outages[host] = new Date();
-            }
-
-            return chalk.blue.bold(host) + chalk.grey(" - ") + chalk.red("Outage Started") + chalk.grey(" - ") + chalk.red(outages[host].toLocaleTimeString());
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
         }
-    }));
+        if (stderr) {
+            console.error(stderr);
+            return;
+        }
 
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(results.join(" | "));
-}, 1000);
+        console.log(chalk.green("Updated"));
+    });
+
+} else {
+    const hosts = myArgs.length ? myArgs : ['1.1.1.1', '8.8.8.8'];
+    /**
+     * @type {{[key: string]: Date}}
+     */
+    let outages = {};
+
+    console.log(chalk.green("Starting"), chalk.blue((new Date()).toLocaleTimeString()));
+    console.log(...hosts.map(host => chalk.blue(host)));
+    console.log("");
+
+    setInterval(async () => {
+
+        const results = await Promise.all(hosts.map(async host => {
+            try {
+
+                const result = await ping.promise.probe(host, {
+                    timeout: 0.8
+                });
+
+                if (!result.alive) {
+                    throw new Error("Down");
+                }
+
+
+                if (outages[host]) {
+                    // calculate the outage time
+                    let elapsed = (new Date()).getTime() - outages[host].getTime();
+
+                    console.log("");
+                    console.log(chalk.blue(host), chalk.grey("out for"), chalk.red(Math.round(elapsed / 1000)), chalk.grey("seconds"));
+                    outages[host] = null;
+                }
+
+                return chalk.blue.bold(host) + chalk.grey(" - ") + chalk.green(result.time + "ms");
+
+            } catch (err) {
+                // ping failed
+                if (!outages[host]) {
+                    outages[host] = new Date();
+                }
+
+                return chalk.blue.bold(host) + chalk.grey(" - ") + chalk.red("Outage Started") + chalk.grey(" - ") + chalk.red(outages[host].toLocaleTimeString());
+            }
+        }));
+
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(results.join(" | "));
+    }, 1000);
+
+}
